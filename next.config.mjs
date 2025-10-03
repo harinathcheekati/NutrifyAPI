da181ef624413b3cb53f4e635a1242a96368ab6b
Hari C <harinath29@gmail.com>
2025-10-03 02:50:41 +0000

    why 404 ?

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...config.externals, 'firebase-admin'];
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
